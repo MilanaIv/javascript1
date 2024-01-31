1eb61b6b3f76c0773591bd133e3676d1234a94e0
Milana Ivanova <milanaivanova92@gmail.com>
2024-01-31 18:47:33 +0100

    first

---
+++ script.js
@@ -0,0 +1,43 @@
+/*document.body.innerHTML = "<h1>Hello word!</h1>"
+console.log("Hello word")*/
+
+/*document.body.innerHTML = 'Mila' + 'Kuba'
+
+console. log(Math.round(3.3))
+console.log(Math.ceil(4.2))
+console.log(Math.pow(2, 2))
+
+let jmeno = 'milana' //string
+const vek =32 //number
+var jeLector = true //boolean
+console.log(jmeno)
+console.log(vek)
+console.log(jeLector)
+
+
+let pocetHodin = 160
+let hodinovka = 1500
+console.log(pocetHodin * hodinovka)
+
+hodinovka = 1000
+
+console.log(pocetHodin * hodinovka)*/
+
+/*document.body.innerHTML += Math.floor(223 / 60);
+
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 223 % 60;
+
+
+
+document.body.innerHTML = 'Mila' + "." + "Ivanova" + "@mujmail.com"*/
+
+const start = 15
+const delka = 10
+const konec = (start + delka) % 24
+
+document.body.innerHTML = 'Čas konce v hodinách: ' + konec;
+
+document.body.innerHTML = Math.random();
+
+
